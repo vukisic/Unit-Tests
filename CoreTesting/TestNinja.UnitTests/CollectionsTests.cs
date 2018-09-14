3179e63f3baa92b8bb91ca1c1c28f61ad5c90663
vukisic <visicev@gmail.com>
2018-09-14 19:23:43 +0200

    Update

---
+++ CoreTesting/TestNinja.UnitTests/CollectionsTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class CollectionsTests
+    {
+        private TMath _math;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _math = new TMath();
+        }
+
+        [Test]
+        public void Collection_Test_PositiveLimit_ReturnsCoolectionOfOddNums()
+        {
+            var result = _math.GetOddNumbers(10);
+
+            //General
+            //Assert.That(result, Is.Not.Empty);
+            //Assert.That(result.Count(), Is.EqualTo(5));
+
+            //Specific
+            Assert.That(result, Is.EquivalentTo(new[] { 1, 3, 5, 7, 9 }));
+
+            //Sorted
+            Assert.That(result, Is.Ordered);
+
+            //No Duplicates
+            Assert.That(result, Is.Unique);
+        }
+
+        [Test]
+        public void Collection_Test_NegativeLimit_ReturnsCoolectionOfOddNums()
+        {
+            var result = _math.GetOddNumbers(-10);
+
+            //General
+            Assert.That(result, Is.Empty);
+        }
+
+        [Test]
+        public void Collection_Test_ZeroLimit_ReturnsCoolectionOfOddNums()
+        {
+            var result = _math.GetOddNumbers(0);
+
+            //General
+            Assert.That(result, Is.Empty);
+        }
+    }
+}
---
+++ CoreTesting/TestNinja.UnitTests/StringTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class StringTests
+    {
+        [Test]
+        public void String_Test_ReturnsEnclosedString()
+        {
+            var formatter = new HtmlFormatter();
+            var result=formatter.FormatAsBold("Hello World");
+
+            //Specific
+            Assert.That(result, Is.EqualTo("<strong>Hello World</strong>").IgnoreCase);
+
+            //More General
+            Assert.That(result, Does.StartWith("<strong>"));
+            Assert.That(result, Does.EndWith("</strong>"));
+            Assert.That(result, Does.Contain("Hello World"));
+        }
+    }
+}
